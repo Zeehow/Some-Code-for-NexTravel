da0f4bf8a3d173bbead4dd306cad30c5546b0a94
Zhehao Cai <zhehaoca@usc.edu>
2018-07-09 12:47:51 -0700

    before adding dest

--- regModel.py
+++ regModel.py
@@ -19,8 +19,37 @@ from dateutil import parser
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
+def varTest (trainbase, iterations):
+    percDf = pd.DataFrame()
+    for i in trainbase:
+        percArr = []
+        traindata = trainbase[[i]]
+        for j in range(iterations):
+            train, test, trainTar, testTar = train_test_split(traindata, target, test_size = 0.2)
+            gbmodel = GradientBoostingClassifier(n_estimators=100)
+            gbmodel.fit(train, trainTar)
+            predictions = gbmodel.predict_proba(test)
+            result = []
+            for prediction in predictions[:,0]:
+                if prediction < 0.5:
+                    result.append(1)
+                else:
+                    result.append(0)
+            fault = 0
+            for k in range(len(testTar)):
+                 if testTar[k] != result[k]:
+                 #if testTar[k] == 0:
+                     fault = fault + 1
+            perc = fault/len(testTar)
+            percArr.append(perc)
+        percDf[str(i)] = (percArr)
+    return percDf
+    
+#def genVar (varSeries):
+#    varSeries.fillna(0)
+    
 
-base = pd.read_csv("E:/NexTravel/withEmail.csv")
+base = pd.read_csv("E:/NexTravel/withGeo.csv")
 res = pd.read_csv("E:/NexTravel/churnVal.csv")
 truF = base['name'].isin(res['name'])
 cnt = 0
@@ -65,6 +94,7 @@ cnt1 = 0
 pos = 0
 base = base.reset_index()
 base = base.drop(['index'], axis = 1)
+
 for i in base['industryLevel1']:
     if not pd.isnull(i):
         if i != rec1:
@@ -81,14 +111,46 @@ for i in base['industryLevel1']:
         pos = pos + 1
     else:
         arr0.append(0)
+        
 base['size'] = base['size'].fillna(0)
 base['emails'] = base['emails'].fillna(0)
 base['i0'] = arr0
 base['i1'] = arr1
+
+base = base.sort_values(['geoCountryCode'])
+gccNumber = 1
+gccRec = -1
+gccArr = []
+for i in base['geoCountryCode']:
+    if not pd.isnull(i):
+        if i != gccRec:
+            gccNumber = gccNumber + 1
+        gccArr.append(gccNumber)
+    else:
+        gccArr.append(0)
+base['gccNum'] = gccArr
+base = base.reset_index()
+base = base.drop(['index'], axis = 1)
+
+base = base.sort_values(['geoCity'])
+gcNumber = 1
+gcRec = -1
+gcArr = []
+for i in base['geoCity']:
+    if not pd.isnull(i):
+        if i != gcRec:
+            gcNumber = gcNumber + 1
+        gcArr.append(gcNumber)
+    else:
+        gcArr.append(0)
+base['gcNum'] = gcArr
+base = base.reset_index()
+base = base.drop(['index'], axis = 1)
+
 base['cpero'] = base['calls']/base['orders']
 base['epero'] = base['emails']/base['orders']
 target = []
-traindata = base[['size', 'i0', 'i1', 'cpero','epero']]
+traindata = base[['size', 'i0','i1','cpero','epero','gccNum']]
 for rows in base.itertuples():
     if rows.Y2 == 1 or rows.Y2 == 2 or rows.Y2 == 3:
         if rows.Y3 == 0:
@@ -107,6 +169,8 @@ for rows in base.itertuples():
             target.append(0)
         else:
             target.append(1)
+            
+#Get list of churn Companies
 listChurn = []
 for ite in range(len(target)):
     if target[ite] == 0:
@@ -123,11 +187,23 @@ for prediction in predictions[:,0]:
     else:
         result.append(0)
 fault = 0
-#rand = np.random.randint(2, size = len(testTar))
+
 for i in range(len(testTar)):
      if testTar[i] != result[i]:
-     #if testTar[i] == 0:
          fault = fault + 1
+
+base = base
+churnY2 = (base[base['Y2'] == 0]['ci'])
+churnY3 = (base[base['Y3'] == 0]['ci'])
+churnY4 = (base[base['Y4'] == 0]['ci'])
+x_coord = []
+for i in range(len(testTar)):
+    x_coord.append(i/len(testTar))
+
+plt.scatter(x_coord, predictions[:,1], c = testTar)
+plt.fill([0,1,1,0],[0,0,0.5,0.5], c='#ff00ff80')
+plt.fill([0,1,1,0],[0.5,0.5,1,1], c='#ffff0080')
+plt.show()
 
 perc = fault/len(testTar)
 testperc = 0
@@ -135,3 +211,5 @@ for i in target:
     if i != 1:
         testperc = testperc + 1
 testperc = testperc/len(target)
+
+testRes = varTest(traindata, 10)
